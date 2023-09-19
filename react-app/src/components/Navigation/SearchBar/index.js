a5ea46fe406dd38645420c6339575fb8fc0083b3
Jennifer Langley <jlangley1019@gmail.com>
2023-09-19 12:17:33 -0400

    css update

--- react-app/src/components/Navigation/SearchBar/index.js
+++ react-app/src/components/Navigation/SearchBar/index.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 const SearchBar = () => {
+    const [search, setSearch] = useState("")
+
+    const products = useSelector(state => state.products);
+    let filteredProducts;
+    useEffect(() => {
+        filteredProducts = Object.values(products).filter(product => product.name.includes(search))
+        console.log(search)
+    }, [search])
+
     return (
         <div className="searchBarContainer">
             <form className="searchBarForm">
@@ -7,8 +19,12 @@ const SearchBar = () => {
                     type="text"
                     className="searchBarInput"
                     placeholder="Search for items"
+                    value={search}
+                    onChange={(e) => setSearch(search)}
                 >
-
+                {filteredProducts && <ul>
+                    {filteredProducts.map(product => product.name)}
+                </ul>}
                 </input>
             </form>
         </div>
