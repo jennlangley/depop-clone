52f46c7347a3107f0cf3b0acf3a846735d199e63
Jennifer Langley <jlangley1019@gmail.com>
2023-09-30 19:13:16 -0400

    landing page css

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,12 +1,29 @@
 
 import './LandingPage.css';
+import womens from '../../images/womens.jpg';
+import mens from '../../images/mens.jpg';
+import accessories from '../../images/accessories.jpg';
 const LandingPage = () => {
     
     
     return (
         <div className='landingPage'>
-            <div>
-                {/* <img src="" /> */}
+            <div className="discoverBox">
+                Discover new fashion
+            </div>
+            <div className="categoryImageLinks">
+                <div className="imageLinkContainer">
+                    <div className="nameDiv"><span className="name">Shop Men's</span></div>
+                    <img className='categoryImage' alt="accessories" src={mens} />
+                </div>
+                <div className="imageLinkContainer">
+                    <div className="nameDiv"><span className="name">Shop Women's</span></div>
+                    <img className='categoryImage' alt="accessories" src={womens} />
+                </div>
+                <div className="imageLinkContainer">
+                    <div className="nameDiv"><span className="name">Shop Accessories</span></div>
+                    <img className='categoryImage' alt="accessories" src={accessories} />
+                </div>
             </div>
         </div>
     )
