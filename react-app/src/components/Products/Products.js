1e7cff7980e7b5b8dde9895ca2a4f6bf61a96d1f
Jennifer Langley <jlangley1019@gmail.com>
2024-06-25 11:15:50 -0400

    filters css edit

--- react-app/src/components/Products/Products.js
+++ react-app/src/components/Products/Products.js
@@ -2,19 +2,41 @@
 import Filters from "../Filters/Filters";
 import ProductTile from "./ProductTile";
 import Breadcrumb from "../ProductCategory/Breadcrumb";
+import { useEffect, useState } from "react";
 
 const Products = ({ products }) => {
-    
+    const [productsList, setProductsList] = useState(products);
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [condition, setCondition] = useState('');
+    const [size, setSize] = useState('');
+
+    useEffect(() => {
+        
+    }, [price, category, condition, size])
+
     return (
-        products &&
+        productsList &&
         <div>
             <Breadcrumb />
+            
             <div className="productsFilterContainer">
 
-                {/* <Filters /> */}
+                <div className="productFilters">
+                    <div className="productFilter">
+                        <label>Sort by</label>
+                        <select>
+                            <option value="" disabled selected>Price</option>
+                            <option>Lowest to Highest</option>
+                            <option>Highest to Lowest</option>
+                        </select>
+                    </div>
+                </div>
 
                 <div className="productsList">
-                {Object.values(products).map((product, idx) => 
+
+                
+                {Object.values(productsList).map((product, idx) => 
                     <ProductTile key={idx} product={product} />)
                 }
                 </div>
